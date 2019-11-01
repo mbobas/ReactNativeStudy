c8b3b518b952b3663479b97c114e28bc8bfb7491
mbobas <mbobas@gmail.com>
2019-11-01 23:09:59 +0100

    ReactNativeStudy2
    Begining of udemy lessons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>HomeScreen</Text>
-      <Button title="Layout" onPress={() => navigation.navigate('Layout')} />
-    </View>
+      <Text style={styles.text}>HomeScreen2</Text>
+      <Button 
+          onPress={() => console.log('Butto pressed')}
+          title="Go to Components Demo" 
+          />
+          <TouchableOpacity onPress={() => console.log('List pressed')}>
+            <Text>Go to List Demo</Text>
+          </TouchableOpacity>
+
+      </View>
   );
 };
 
-HomeScreen.navigationOptions = {
-  title: 'Home'
-};
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
 
 export default HomeScreen;

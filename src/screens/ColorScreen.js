8c4d56177052890275cdc9b2cfb64644a55ffc69
mbobas <mbobas@gmail.com>
2019-11-02 12:06:24 +0100

    ShowingColorInFlatList

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,5 +1,5 @@
 import React, { useState }from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button,  FlatList } from 'react-native';
 
 const ColorScreen = () => {
 const [colors, setColors] = useState([]);
@@ -13,10 +13,18 @@ console.log(colors);
                     setColors([...colors, randomRgb()]) // get an array and add new color, not clear an array
                 }}
                 />
-            <View 
-                style={{height: 100, width: 100, backgroundColor: randomRgb() }}
             
+            <FlatList
+            keyExtractor={(item) => item}
+            data={colors}
+            renderItem={({ item }) => {
+                return <View 
+                style={{height: 100, width: 100, backgroundColor: item }}
             />
+            
+            }}
+            />
+
         </View>
     );
 

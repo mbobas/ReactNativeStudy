bffff2611e5e1685ea0d167500fc6d47cadbf36d
mbobas <mbobas@gmail.com>
2019-11-02 13:29:47 +0100

    ValidationStateChange_v1

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -8,14 +8,28 @@ const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
+
+    const setColor = (color, change) => {
+        //color === 'red', 'blue, 'green,
+        //change === + 15 or -15
+        if (color === 'red') {
+            if(red + change > 255 || red + change < 0){
+                return ;
+            }else {
+                setRed(red + change);
+            }
+        }
+        
+
+    };
     
     //console.log(red);
 
     return(
         <View>
             <ColorCounter 
-                onIncrease={() => setRed(red + COLOR_INCREMENT)} 
-                onDecrease={() => setRed(red - COLOR_INCREMENT)}
+                onIncrease={() => setColor('red', COLOR_INCREMENT)} 
+                onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
                 color="Red" 
             />
 

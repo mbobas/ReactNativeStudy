1d074af327c72d2022508799631ed5455c876814
mbobas <mbobas@gmail.com>
2019-11-02 11:43:58 +0100

    addinRandomColors in ColorScreen

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { View, StyleSheet, Button } from 'react-native';
 
 const ColorScreen = () => {
+const [colors, setColors] = useState([]);
+console.log(colors);
+
     return (
         <View>
-            <Button title="Add a Color" />
+            <Button 
+                title="Add a Color"
+                onPress={() => {
+                    setColors([...colors, randomRgb()]) // get an array and add new color, not clear an array
+                }}
+                />
             <View 
                 style={{height: 100, width: 100, backgroundColor: randomRgb() }}
             

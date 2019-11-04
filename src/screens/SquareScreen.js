da0d21be79e8d5598e0c64b1baba15afd41506fd
mbobas <mbobas@gmail.com>
2019-11-04 22:13:43 +0100

    ConventionInReducerCommunity - changing namesOfVariables

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,24 +7,24 @@ const COLOR_INCREMENT = 15;
 //konwencja mówi, że reduce powinien być poza componentem, ze względu na state
 const reducer = (state, action) => {
 // state. here is === { red: number, green: number, blue: number};
-//action. her is === {colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15};
+// action. here is === {type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15};
 
-    switch(action.colorToChange) {
+    switch(action.type) {
         //never going to do:    state.red = state.red -15 (example);
-        case 'red': 
-            return state.red + action.amount > 255 || state.red + action.amount < 0 
+        case 'change_red': 
+            return state.red + action.payload > 255 || state.red + action.payload < 0 
                 ? state
-                : { ...state, red: state.red + action.amount };
+                : { ...state, red: state.red + action.payload };
 
-        case 'green':
-            return state.green + action.amount > 255 || state.green + action.amount < 0 
+        case 'change_green':
+            return state.green + action.payload > 255 || state.green + action.payload < 0 
                 ? state
-                : { ...state, green: state.green + action.amount };
+                : { ...state, green: state.green + action.payload };
 
-        case 'blue':
-           return state.blue + action.amount > 255 || state.blue + action.amount < 0 
+        case 'change_blue':
+           return state.blue + action.payload > 255 || state.blue + action.payload < 0 
                 ? state
-                : { ...state, blue: state.blue + action.amount };
+                : { ...state, blue: state.blue + action.payload };
         default:
             return state;
     }
@@ -40,20 +40,20 @@ const SquareScreen = () => {
     return(
         <View>
             <ColorCounter 
-                onIncrease={() => dispatch({colorToChange: 'red', amount: COLOR_INCREMENT})} 
-                onDecrease={() => dispatch({colorToChange: 'red', amount: -1 * COLOR_INCREMENT})} 
+                onIncrease={() => dispatch({type: 'change_red', payload: COLOR_INCREMENT})} 
+                onDecrease={() => dispatch({type: 'change_red', payload: -1 * COLOR_INCREMENT})} 
                 color="Red" 
             />
 
             <ColorCounter 
-                onIncrease={() => dispatch({colorToChange: 'blue', amount: COLOR_INCREMENT})} 
-                onDecrease={() => dispatch({colorToChange: 'blue', amount: -1 * COLOR_INCREMENT})}
+                onIncrease={() => dispatch({type: 'change_blue', payload: COLOR_INCREMENT})} 
+                onDecrease={() => dispatch({type: 'change_blue', payload: -1 * COLOR_INCREMENT})}
                 color="Blue" 
             />
 
             <ColorCounter 
-                onIncrease={() => dispatch({colorToChange: 'green', amount: COLOR_INCREMENT})} 
-                onDecrease={() => dispatch({colorToChange: 'green', amount: -1 * COLOR_INCREMENT})}
+                onIncrease={() => dispatch({type: 'change_green', payload: COLOR_INCREMENT})} 
+                onDecrease={() => dispatch({type: 'change_green', payload: -1 * COLOR_INCREMENT})}
                 color="Green" 
             />
             <View style={{height:150, width:150, backgroundColor: `rgb(${red},${green},${blue})`}}>
